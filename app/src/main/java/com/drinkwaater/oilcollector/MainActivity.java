f7fe7f9b71642964e8ea90db27f8c370c2f7ebbb
Goddamnnoob <gowthamreddy547@gmail.com>
2020-05-07 13:29:09 +0530

    Initial commit

---
+++ app/src/main/java/com/drinkwaater/oilcollector/MainActivity.java
@@ -0,0 +1,14 @@
+package com.drinkwaater.oilcollector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
