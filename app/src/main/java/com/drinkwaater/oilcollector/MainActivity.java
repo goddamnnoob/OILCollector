6b2fc32914faefed2dfeb482fb0cf5845f8b6d7f
Goddamnnoob <gowthamreddy547@gmail.com>
2020-05-18 19:26:04 +0530

    stage 2

--- app/src/main/java/com/drinkwaater/oilcollector/MainActivity.java
+++ app/src/main/java/com/drinkwaater/oilcollector/MainActivity.java
@@ -3,9 +3,10 @@ package com.drinkwaater.oilcollector;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
